cd0832d233a279bbb67f9f0860dfd1db07c0ff9b
phlf <ph.lefur@gmail.com>
2015-03-30 10:54:04 +0200

    fast_math.c finie
    Version finale et fonctionnelle des méthodes de calcul trigos rapides.

--- fast_math.c
+++ fast_math.c
@@ -1,19 +1,55 @@
 #include "Talkthrough.h"
 
-fract16 fast_sin(fract16 x){
-	fract16 y;
+long fract fast_sin(long accum x){
+	long fract y;
+	int high_pow_x;
 	
+	//Is x between -PI and +PI?
+	if(x>3.141592654)
+	{
+		x=x-6.283185307;
+	}
+	else if(x<-3.141592654)
+	{
+		x=x+6,283185307;
+	}
+	
+	//This is necessary because accumulator only goes to 256.
+	if(x>2.20815||x<-2.20815)
+	{
+		high_pow_x=(int)(x*x*x*x*x*x*x);
+	}
+	
+	y=x-((x*x*x)/fast_factorial(3))+((x*x*x*x*x)/fast_factorial(5))-(high_pow_x/fast_factorial(7));
 	return y;
 }
 
-fract16 fast_cos(fract16 x){
-	fract16 y;
+long fract fast_cos(long accum x){
+	long fract y;
+	int high_pow_x;
 	
+	//Is x between -PI and +PI?
+	if(x>3.141592654)
+	{
+		x=x-6.283185307;
+	}
+	else if(x<-3.141592654)
+	{
+		x=x+6,283185307;
+	}
+	
+	//This is necessary because accumulator only goes to 256.
+	if(x>2.51983||x<-2.51983)
+	{
+		high_pow_x=(int)(x*x*x*x*x*x);
+	}
+	
+	y=1-((x*x)/fast_factorial(2))+((x*x*x*x)/fast_factorial(4))-(high_pow_x/fast_factorial(6));
 	return y;
 }
 
 //Max unsigned int value for ASDP-BF533 is 4294967295.
-unsigned int fast_factorial(unsigned int i){
+unsigned int fast_factorial(unsigned short i){
 	unsigned int array_factorial[12]={
 		1,
 		1,
@@ -30,24 +66,4 @@ unsigned int fast_factorial(unsigned int i){
 		479001600};
 
 		return array_factorial[i];
-}
-
-//Max unsigned int value for ASDP-BF533 is 4294967295.
-unsigned int fast_power(unsigned int i){
-	unsigned int array_power[12]={
-		1,
-		1,
-		2,
-		6,
-		24,
-		120,
-		720,
-		5040,
-		40320,
-		362880,
-		3628800,
-		39916800,
-		479001600};
-
-		return array_powerl[i];
 }
